60e4d87417602686aecd4a38e594124c5b16d483
freddytc <u20202269@utp.edu.pe>
2024-12-06 14:20:52 -0500

    Uploading

--- src/main/java/com/example/backend/Configuration/SecurityConfig.java
+++ src/main/java/com/example/backend/Configuration/SecurityConfig.java
@@ -3,19 +3,24 @@ package com.example.backend.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()
+                .cors() // Habilitar CORS
+                .and()
+                .csrf().disable() // Deshabilitar CSRF si no es necesario
                 .authorizeHttpRequests()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll(); // Permitir todas las solicitudes
+
         return http.build();
     }
 
